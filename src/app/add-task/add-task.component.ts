c13db95b9597f61cc7e0e77c6afcb9755996d7f3
GomathyRangan <50116511+GomathyRangan@users.noreply.github.com>
2019-06-17 07:42:25 +0530

    latest changes
    -

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -4,51 +4,72 @@ import { Router } from '@angular/router';
 import { TaskService } from '../services/task.service';
 
 @Component({
-  selector: 'app-add-task',
-  templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+	selector: 'app-add-task',
+	templateUrl: './add-task.component.html',
+	styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private taskService: TaskService) { }
+	constructor(private formBuilder: FormBuilder, private router: Router, private taskService: TaskService) { }
 
-    addTaskForm: FormGroup;
+	addTaskForm: FormGroup;
 
-    ngOnInit() {
+	ngOnInit() {
 
-        this.addTaskForm = this.formBuilder.group({
-            taskId: [],
-            parentId: ['', Validators.required],
-            projectId: ['', Validators.required],
-            task: ['', Validators.required],
-            priority: ['', Validators.required],
-            startDate: ['', Validators.required],
-            endDate: ['', Validators.required],
-            userId: ['', Validators.required]
-        });
+		this.addTaskForm = this.formBuilder.group({
+			taskId: [],
+			parentId: ['', Validators.required],
+			projectId: ['', Validators.required],
+			task: ['', Validators.required],
+			priority: ['', Validators.required],
+			startDate: ['', Validators.required],
+			endDate: ['', Validators.required],
+			userId: ['', Validators.required]
+		});
 
 	}
 
-	   get formControls() {
-        return this.addTaskForm
-            .controls;
-    }
+	get formControls() {
+		return this.addTaskForm
+			.controls;
+	}
 
-    error: any = { isError: false, errorMessage: '' };
-	
-		 compareTwoDates() {
-        if (new Date(this.addTaskForm.controls['endDate'].value) < new Date(this.addTaskForm.controls['startDate'].value)) {
-            this.error = {
-                isError: true, errorMessage: 'End Date shoule be greater than start date.'
-            };
-        }
-    }
+	error: any = { isError: false, errorMessage: '' };
 
-    onSubmit() {
-        this.taskService.createTask(this.addTaskForm.value)
-            .subscribe(data => {
-                console.log("Add-TAsk After Create: ", JSON.stringify(data));
-                this.router.navigate(['add-task']);
-            });
-    }
+	compareTwoDates() {
+		if (new Date(this.addTaskForm.controls['endDate'].value) < new Date(this.addTaskForm.controls['startDate'].value)) {
+			this.error = {
+				isError: true, errorMessage: 'End Date shoule be greater than start date.'
+			};
+		}
+	}
+
+	onSubmit() {
+		if (this.addTaskForm.controls['task'].value === '') {
+			this.error = {
+				isError: true, errorMessage: 'Task Name is required.'
+			};
+			return;
+		} else if (this.addTaskForm.controls['startDate'].value === '' || this.addTaskForm.controls['endDate'].value === '') {
+
+			this.error = {
+				isError: true, errorMessage: 'Enter value for both Start Date and end Date'
+			};
+			return;
+		} else if (new Date(this.addTaskForm.controls['endDate'].value) < new Date(this.addTaskForm.controls['startDate'].value)) {
+			this.error = {
+				isError: true, errorMessage: 'End Date shoule be greater than start date.'
+			};
+
+		} else {
+			this.error = {
+				isError: false, errorMessage: ''
+			};
+			this.taskService.createTask(this.addTaskForm.value)
+				.subscribe(data => {
+					console.log("Add-TAsk After Create: ", JSON.stringify(data));
+					this.router.navigate(['add-task']);
+				});
+		}
+	}
 }
--- src/app/edit-task/edit-task.component.ts
+++ src/app/edit-task/edit-task.component.ts
@@ -4,37 +4,69 @@ import { Router } from '@angular/router';
 import { TaskService } from '../services/task.service';
 
 @Component({
-  selector: 'app-edit-task',
-  templateUrl: './edit-task.component.html',
-  styleUrls: ['./edit-task.component.css']
+	selector: 'app-edit-task',
+	templateUrl: './edit-task.component.html',
+	styleUrls: ['./edit-task.component.css']
 })
 export class EditTaskComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private taskService: TaskService) { }
+	constructor(private formBuilder: FormBuilder, private router: Router, private taskService: TaskService) { }
 
-    editTaskForm: FormGroup;
+	editTaskForm: FormGroup;
 
-    ngOnInit() {
+	error: any = { isError: false, errorMessage: '' };
 
-        this.editTaskForm = this.formBuilder.group({
-            taskId: [],
-            parentId: ['', Validators.required],
-            projectId: ['', Validators.required],
-            task: ['', Validators.required],
-            priority: ['', Validators.required],
-            startDate: ['', Validators.required],
-            endDate: ['', Validators.required],
-            userId: ['', Validators.required]
-        });
+	compareTwoDates() {
+		if (new Date(this.editTaskForm.controls['endDate'].value) < new Date(this.editTaskForm.controls['startDate'].value)) {
+			this.error = {
+				isError: true, errorMessage: 'End Date shoule be greater than start date.'
+			};
+		}
+	}
 
-    }
+	ngOnInit() {
 
-    onSubmit() {
-        this.taskService.editTask(this.editTaskForm.value)
-            .subscribe(data => {
-                console.log("Edit-TAsk After Create: ", JSON.stringify(data));
-                this.router.navigate(['view-task']);
-            });
-    }
+		this.editTaskForm = this.formBuilder.group({
+			taskId: [],
+			parentId: ['', Validators.required],
+			projectId: ['', Validators.required],
+			task: ['', Validators.required],
+			priority: ['', Validators.required],
+			startDate: ['', Validators.required],
+			endDate: ['', Validators.required],
+			userId: ['', Validators.required]
+		});
+
+	}
+
+	onSubmit() {
+
+		if (this.editTaskForm.controls['task'].value === '') {
+			this.error = {
+				isError: true, errorMessage: 'Task Name is required.'
+			};
+			return;
+		} else if (this.editTaskForm.controls['startDate'].value === '' || this.editTaskForm.controls['endDate'].value === '') {
+
+			this.error = {
+				isError: true, errorMessage: 'Enter value for both Start Date and end Date'
+			};
+			return;
+		} else if (new Date(this.editTaskForm.controls['endDate'].value) < new Date(this.editTaskForm.controls['startDate'].value)) {
+			this.error = {
+				isError: true, errorMessage: 'End Date shoule be greater than start date.'
+			};
+
+		} else {
+			this.error = {
+				isError: false, errorMessage: ''
+			};
+			this.taskService.editTask(this.editTaskForm.value)
+				.subscribe(data => {
+					console.log("Edit-TAsk After Create: ", JSON.stringify(data));
+					this.router.navigate(['view-task']);
+				});
+		}
+	}
 
 }
